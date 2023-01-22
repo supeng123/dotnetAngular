722c9b498487bd9823fe676d2faa54f650a673ca
supeng123 <492526873@qq.com>
2023-01-22 13:51:29 +1300

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase {
+        public IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() {
+            var products =  await _repo.GetProductsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes() {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+    }
+}
---
+++ start.md
@@ -0,0 +1,53 @@
+### step one to set up a project
+~~~
+1. mkdir skinet
+2. cd skinet
+3. dotnet new slt
+4. dotnet new webapi -o API
+5. dotnet sln add API/
+6. code .
+7. cd API
+8. dotnet run
+9. dotnet dev-certs https -t
+~~~
+
+### check add libs
+~~~
+dotnet --info
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 6.0.8
+dotnet restore
+
+dotnet tool install --global dotnet-ef --version 6.0.8
+dotnet tool list -g
+
+dotnet ef -h
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 6.0.8
+dotnet ef migrations add InitialCreate -o Data/Migrations
+dotnet ef database update
+~~~
+
+### new extra libs
+~~~
+dotnet new classlib -o Core
+dotnet new classlib -o Infrastructure
+
+dotnet sln add Core/
+dotnet sln add Infrastructure/
+
+cd API
+dotnet add reference ../Infrastructure
+
+cd ..
+cd Infrastructure
+dotnet add reference ../Core
+
+cd..
+dotnet restore
+~~~
+### migration DB
+~~~
+dotnet ef database drop -p Infrastructure -s API
+dotnet ef migrations remove -p Infrastructure -s API
+
+dotnet ef migrations add InitialCreate -p Infrastructure -s API -o Data/Migrations
+~~~
